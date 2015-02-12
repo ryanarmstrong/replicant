313f3bc32f7dfad19bbeff44ca06965fcd706e61
ryanarmstrong <jenova49@gmail.com>
2015-02-11 16:51:03 -0800

    Updated post-install.sh.

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -11,10 +11,25 @@ do
   sed -i "s/require_once\ __DIR__\ \.\ '\/vendor\/autoload\.php'\;/require_once\ __DIR__\ \.\ '\/..\/..\/vendor\/autoload\.php'\;/" $FILE
 done
 
-# Copy index.php from the upstream Drupal repository and modify to use the proper autoloader.
+# Download needed files that reside outside of Drupal core.
 cd ${ROOT_DIR}/web
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/.csslintrc
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/.editorconfig
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/.eslintignore
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/.eslintrc
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/.gitattributes
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/.htaccess
 wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/index.php
-perl -pi -e 's/core/../g' index.php
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/robots.txt
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/web.config
+
+mkdir -p ${ROOT_DIR}/web/sites/default
+cd ${ROOT_DIR}/web/sites/default
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/sites/default/default.services.yml
+wget https://raw.githubusercontent.com/drupal/drupal/8.0.x/sites/default/default.settings.php
+
+# Modify settings.php to use the correct configuration staging folder.
+perl -pi -e 's/core/../g' ${ROOT_DIR}/web/index.php
 
 # Get Drupal Console
 curl -LSs http://drupalconsole.com/installer | php
